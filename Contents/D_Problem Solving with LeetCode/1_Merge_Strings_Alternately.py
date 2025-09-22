7e61534eaa03ad09a71b77071f3a8de3032e95b7
habiba033 <habibanasser033@gmail.com>
2025-09-22 08:43:13 +0300

    updating

--- Contents/D_Problem Solving with LeetCode/1_Merge_Strings_Alternately.py
+++ Contents/D_Problem Solving with LeetCode/1_Merge_Strings_Alternately.py
@@ -3,9 +3,3 @@ You are given two strings word1 and word2. Merge the strings by adding letters i
 If a string is longer than the other, append the additional letters onto the end of the merged string.
 
 Return the merged string."""
-
-word1 = input("enter the first word :")
-word2 = input("enter the second word :")
-i=0
-for i in range(len(word1)):
-    print("!")
